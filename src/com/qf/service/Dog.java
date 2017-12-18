2fff1592accdd9a2bb5cd6f168fc5aa27334ef78
guosihua <1290641779@qq.com>
2017-12-18 15:59:13 +0800

    Initial commit

---
+++ src/com/qf/service/Dog.java
@@ -0,0 +1,12 @@
+package com.qf.service;
+
+public class Dog {
+    String name;
+    int age;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
